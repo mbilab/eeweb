17e9cfd04bbf631d49bdcce8cfca170187883eed
low0621 <low0621@gmail.com>
2017-08-04 09:12:47 +0800

    chore(app): add favicon

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,9 +8,9 @@ import opt from './option.json'
 export default {
   context: resolve('app'),
   devServer: {
-    host: opt.host,
+    host: opt[0].host,
     inline: true,
-    port: opt.port,
+    port: opt[0].port,
     stats: { chunkModules: false }
   },
   entry: './app.js',
@@ -18,7 +18,7 @@ export default {
     rules: [
       { test: /\.css$/, use: ['style-loader?insertAt=top', 'css-loader'] },
       { test: /\.(eot|otf|svg|ttf|woff(2)?)(\?[a-z0-9]+)?$/, use: 'file-loader?name=fonts/[hash:7].[ext]' },
-      { test: /\.(jpeg|jpg|png)$/, use: 'url-loader?limit=10000' },
+      { test: /\.(jpeg|jpg|png|ico)$/, use: 'url-loader?limit=10000' },
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
       { test: /\.pug$/, use: ['file-loader?name=[name].html', 'extract-loader', 'html-loader', 'pug-html-loader?exports=false'] },
       { test: /\.sass$/, use: ['file-loader?name=[name].css', 'extract-loader', 'css-loader', { loader: 'postcss-loader', options: { plugins: [autoprefixer] } }, 'sass-loader'] }
