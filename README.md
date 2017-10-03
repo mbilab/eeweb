c3946ea80246a4075bb76af4e982b23665109704
dirty <darby.chang@gmail.com>
2017-10-03 09:20:26 +0800

    code suggestion

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ eeweb
 
 # Develop
 - npm start
+
+# Issue
+* no need #announcement
+* mustache in list
+* font serif?
+* item bottom margin bug while resizing
+* .description top margin while resizing
+* rename .page-info .theme to .page-info .title
+* #menu font size speed while resizing
+* icon, bg to local?
+* d3 in webpack or remove
+* rename #main to #page
+* scroll bar hidden by default
+* use real (chinese) articles
--- server.js
+++ server.js
@@ -6,48 +6,54 @@ const app = express()
 const path = require('path')
 const fs = require('fs')
 
-//HMR setting
-const webpack = require('webpack')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const webpackConfig = require('./webpack.config.babel.js')
-const compiler = webpack(webpackConfig)
-
 //npm module
 const moment = require('moment')
 
 //lib 
 const dp = require('./lib/dropbox.js')
 
+//process data from dropbox paper and save it to the front end
+dp.data(it => {
+
+  let data = it.split(/\n\n\-{10}\n/).slice(1, -1)
+  let moment = require('moment')
+  let news = []
+
+  for (let v of data) {
+    let match = v.match(/##\s*(.+?)\s*\n\s*(.+?)\s*\n\s*([\s\S]+?)\s*$/)
+    let date = null
+    if (moment(match[2]).isValid())
+      date = moment(match[2])
+    else
+      match[3] = `${match[2]}\n${match[3]}`
+    news.push({
+      title: match[1],
+      date: date,
+      content: match[3],
+    })
+  }
+
+  fs.writeFileSync('./app/res/data.json', JSON.stringify({
+    news: news
+  }, null, 2))
+})    
+
+/*
+process.exit(0)
+
+//HMR setting
+const webpack = require('webpack')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const webpackConfig = require('./webpack.config.babel.js')
+const compiler = webpack(webpackConfig)
+
 //express
 app.listen(1098, ()=>{console.log("server connect")})
 
 app.use(require('webpack-dev-middleware')(compiler, {
-      noInfo: true, 
-      publicPath: webpackConfig.output.publicPath
+  noInfo: true, 
+  publicPath: webpackConfig.output.publicPath
 }))
-    
+
 app.use(webpack_hot_middleware = webpackHotMiddleware(compiler))
-
-//process data from dropbox paper and save it to the front end
-dp.data( it => {
-
-let data = it.split(/\-{10}/).filter( e => { return e }).slice(1,-1)
-let output = {}
-let moment = require('moment')
-let obj = []
-let date
-
-output["news"] = []
-obj = output["news"]
-
-for(let i = 0; i< data.length; ++i){
-  let title = data[i].match(/##(.)+/g)[0].replace(/^##\s*/,"")
-  let arti = data[i].replace(/##(.)+/,"").split(/\n/).filter( e => { return e })
-  
-  moment(arti[0]).isValid()?(date = arti[0], arti.shift(), obj.push({title: title, date: date, content: arti.join("\n")})):(obj.push({title: title, date: null, content: arti.join("\n")}))
-
-}
-
-fs.writeFileSync("./app/res/data.json",JSON.stringify(output, null, 2))
-
-})    
+*/
