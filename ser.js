3b58c2083248ecb91bee708e50b90764f8f7fcbe
dirty <darby.chang@gmail.com>
2017-10-08 10:22:16 +0800

    chore(eeweb): socket io (no client)

--- ser.js
+++ ser.js
@@ -3,35 +3,59 @@
 const dp = require('./lib/dropbox.js')
 const fs = require('fs')
 const moment = require('moment')
+const opt = require('./option.json')
 
 // parse data from dropbox paper and save it to the front end
 const parseDropbox = it => {
-    let data = it.toString().split(/\n\n\-{10}\n/).slice(1, -1)
-    let news = []
+    it = it.toString().split(/\n\n\-{10}\n/).slice(1, -1)
+    const news = []
     let i = 0
 
-    for (let v of data) {
+    for (let v of it) {
         let match = v.match(/##\s*(.+?)\s*\n\s*(.+?)\s*\n\s*([\s\S]+?)\s*$/)
         let date = null
         if (moment(match[2]).isValid())
             date = moment(match[2]).format('lll')
         else
             match[3] = `${match[2]}\n${match[3]}`
-        i++
         news.push({
-            index: i,
-            title: match[1],
-            date: date,
             content: match[3],
+            date: date,
+            index: i++,
+            title: match[1],
         })
     }
 
-    fs.writeFileSync('./dist/data.json', JSON.stringify({
-        news: news
-    }, null, 2))
+    const data = { news: news }
+    fs.writeFileSync('./dist/data.json', JSON.stringify(data, null, 2))
+
+    return data
 }
 
-//dp.get(parseDropbox) // async
-parseDropbox(dp.getSync()) // sync
+if ('get' === process.argv[2]) {
+    //dp.get(parseDropbox) // async
+    parseDropbox(dp.getSync()) // sync
+} else {
+    const clients = {}
+    const io = require('socket.io')(opt.socket_port)
+    console.log(`socket on ${opt.socket_port}`)
+
+    const refresh = () => {
+        data = parseDropbox(dp.getSync())
+        for (let k in clients)
+            clients[k].emit('data', data)
+    }
+    setInterval(refresh, opt.refresh_interval)
+    io.on('refresh', refresh)
+
+    io.on('connection', socket => {
+        clients[socket.id] = socket
+        socket.emit('data', data)
+    })
+
+    io.on('disconnect', socket => {
+        delete clients[socket.id]
+    })
+}
 
 // vi:et:sw=4:ts=4
